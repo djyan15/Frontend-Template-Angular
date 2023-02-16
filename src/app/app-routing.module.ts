f275898695ad497f1766488891e6418721eb4cf3
Bladimir Rosario Buten <brosario-externo@unir.net>
2023-02-15 23:37:37 -0400

    Cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 
 
 const routes: Routes = [];
-
+// { path: 'name/:id', component: NameComponent },
+// private route: ActivatedRoute
+// this.route.queryParams
+// .subscribe(params => {
+//   console.log(params); // { orderby: "price" }
+//   this.orderby = params.orderby;
+//   console.log(this.orderby); // price
+// }
+// );  this.id = this.route.snapshot.paramMap.get('id')
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
